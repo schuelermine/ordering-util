c40d177de35893d4dbd99d93cf4c552e878ef240
Anselm Schüler <mail@anselmschueler.com>
2022-04-17 14:32:55 +0200

    Add Haddock documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for ordering-util
 
+## 0.1.2.0 — 2022-04-17
+
+* Added Haddock documentation
+
 ## 0.1.1.0 — 2022-04-16
 
 * Lowered required `base` version to 4.14 to support GHC 8.10.1 and up. This also allows the documentation to be built by Hackage.
--- src/Data/Ordering.hs
+++ src/Data/Ordering.hs
@@ -1,25 +1,35 @@
-module Data.Ordering (Ordering(..), ifEQ, noLT, noGT, flipOrdering) where
+module Data.Ordering (
+    -- * Simple functions
+    flipOrdering,
+    ifEQ, noLT, noGT,
+    -- * Reexports from @base@
+    Ordering(..)
+) where
 
 import Data.Ord
 
+-- | Replace the 'EQ' case with another 'Ordering'
 ifEQ :: Ordering -> Ordering -> Ordering
 ifEQ i o =
   case o of
     EQ -> i
     x -> x
 
+-- | Clamp 'LT' to 'EQ'
 noLT :: Ordering -> Ordering
 noLT o =
   case o of
     LT -> EQ
     x -> x
 
+-- | Clamp 'GT' to 'EQ'
 noGT :: Ordering -> Ordering
 noGT o =
   case o of
     GT -> EQ
     x -> x
 
+-- | Flip an 'Ordering'
 flipOrdering :: Ordering -> Ordering
 flipOrdering o =
   case o of
