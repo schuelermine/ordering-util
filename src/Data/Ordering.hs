4df29880b71e54592652bb4f5976f35f54bcf92a
Anselm Schüler <mail@anselmschueler.com>
2022-04-17 19:18:39 +0200

    Add toSign and fromSign

--- src/Data/Ordering.hs
+++ src/Data/Ordering.hs
@@ -2,6 +2,7 @@ module Data.Ordering (
     -- * Simple functions
     flipOrdering,
     ifEQ, noLT, noGT,
+    toSign, fromSign,
     -- * Reexports from @base@
     Ordering(..)
 ) where
@@ -36,3 +37,16 @@ flipOrdering o =
     LT -> GT
     EQ -> EQ
     GT -> LT
+
+-- | Returns -1, 0, and 1 for 'LT', 'EQ', and 'GT', respectively
+toSign :: Num n => Ordering -> n
+toSign o =
+  case o of
+    LT -> -1
+    EQ -> 0
+    GT -> 1
+
+-- | Get an 'Ordering' depending on the sign of a number.
+--   Compares the number to 0.
+fromSign :: (Ord n, Num n) => n -> Ordering
+fromSign o = compare o 0
