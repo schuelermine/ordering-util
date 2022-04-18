d7162064d4967642a1149b0417ba1aa5ca82d6a6
Anselm Schüler <mail@anselmschueler.com>
2022-04-18 12:21:52 +0200

    Add isLE, isNE, isGE, adjustBy, add newline to doc, explicitly specify imports

--- src/Data/Ordering.hs
+++ src/Data/Ordering.hs
@@ -2,12 +2,21 @@ module Data.Ordering (
     -- * Simple functions
     flipOrdering,
     ifEQ, noLT, noGT,
-    toSign, fromSign,
+    isLE, isNE, isGE,
+    toSign, fromSign, adjustBy,
     -- * Reexports from @base@
     Ordering(..)
 ) where
 
-import Data.Ord
+import Data.Ord (Ordering(..))
+
+-- | Flip an 'Ordering'
+flipOrdering :: Ordering -> Ordering
+flipOrdering o =
+  case o of
+    LT -> GT
+    EQ -> EQ
+    GT -> LT
 
 -- | Replace the 'EQ' case with another 'Ordering'
 ifEQ :: Ordering -> Ordering -> Ordering
@@ -30,13 +39,17 @@ noGT o =
     GT -> EQ
     x -> x
 
--- | Flip an 'Ordering'
-flipOrdering :: Ordering -> Ordering
-flipOrdering o =
-  case o of
-    LT -> GT
-    EQ -> EQ
-    GT -> LT
+-- | Check an 'Ordering' represents less-or-equal
+isLE :: Ordering -> Bool
+isLE = (/= GT)
+
+-- | Check an 'Ordering' represents not-equal
+isNE :: Ordering -> Bool
+isNE = (/= EQ)
+
+-- | Check an 'Ordering' represents greater-or-equal
+isGE :: Ordering -> Bool
+isGE = (/= LT)
 
 -- | Returns -1, 0, and 1 for 'LT', 'EQ', and 'GT', respectively
 toSign :: Num n => Ordering -> n
@@ -46,7 +59,16 @@ toSign o =
     EQ -> 0
     GT -> 1
 
--- | Get an 'Ordering' depending on the sign of a number.
---   Compares the number to 0.
+-- | Get an 'Ordering' depending on the sign of a number
+--
+--   Compares the number to 0
 fromSign :: (Ord n, Num n) => n -> Ordering
 fromSign o = compare o 0
+
+-- | Adjust a number by a difference up or down
+adjustBy :: Num a => a -> Ordering -> a -> a
+adjustBy d o x =
+  case o of
+    LT -> x - d
+    EQ -> x
+    GT -> x + d
